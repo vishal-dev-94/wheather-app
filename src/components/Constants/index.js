668b7c5e5b428585946e1c038341a90dd8e7b596
VISHAL <proeffico.vishal@gmail.com>
2023-05-23 00:27:48 +0530

    add to git

---
+++ src/components/Constants/index.js
@@ -0,0 +1,4 @@
+export const apiKey = "c28664c883d8578c4dfe26c1e1a72a9b";
+export const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+// export const apiUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}";
---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { apiKey, apiUrl } from "../../components/Constants/index";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Weather() {
+  const [weather, setWeather] = useState(null);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(`${apiUrl}?q=${query}&appid=${apiKey}`).then((response) => {
+      if (response.status != 200) {
+        console.log("something went wrong");
+        return false;
+      }
+      response.json().then((data) => {
+        console.log(data);
+        setWeather(data);
+      });
+    });
+  }, [query]);
+  return (
+    <div
+      className="card card-body shadow"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <input
+        className="form-control  form-control-sm"
+        style={{ margin: "30px 0" }}
+        onChange={(event) => setQuery(event.target.value)}
+        value={query}
+        type="text"
+        name=""
+        id=""
+        placeholder="Enter City here.."
+      />
+      <div className="row ">
+        <h3 className="text-primary">
+          City: {weather?.name}
+          <img
+            className="cityImg"
+            src="https://cdn.pixabay.com/photo/2018/01/31/05/43/web-3120321_1280.png"
+            alt=""
+          />
+        </h3>
+      </div>
+      <div className="row">
+        <h4 className="text-danger">
+          Temperature: {weather?.main.temp} <sup>o</sup>C
+          <img
+            className="tempImg"
+            src="https://img.freepik.com/premium-vector/celsius-fahrenheit-meteorology-thermometers_53562-2740.jpg?w=740"
+            alt=""
+          />
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
